c86bb9fb9b2710ba711a7aedb74ed4afc764bbeb
Benjamin BALET <benjamin.balet@gmail.com>
2026-03-28 17:07:35 +0100

    Improve fixtures script

--- legacy/application/controllers/Fixtures.php
+++ legacy/application/controllers/Fixtures.php
@@ -39,17 +39,21 @@ class Fixtures extends CI_Controller
         $this->db->query('SET FOREIGN_KEY_CHECKS = 0');
         // Ensure MySQL keeps id = 0
         $this->db->query("SET SESSION sql_mode = CONCAT(@@sql_mode, ',NO_AUTO_VALUE_ON_ZERO')");
-        $this->db->truncate('users');
-        $this->db->truncate('organization');
-        $this->db->truncate('positions');
         $this->db->truncate('contracts');
-        $this->db->truncate('types');
+        $this->db->truncate('dayoffs');
+        $this->db->truncate('delegations');
         $this->db->truncate('entitleddays');
         $this->db->truncate('leaves');
+        $this->db->truncate('oauth_clients');
+        $this->db->truncate('organization');
+        $this->db->truncate('overtime');
+        $this->db->truncate('positions');
+        $this->db->truncate('types');
+        $this->db->truncate('users');
         $this->db->query('SET FOREIGN_KEY_CHECKS = 1');
         echo "Tables truncated.\n";
 
-        // 1. Contracts
+        // Contracts
         $contracts = [
             [
                 'id' => 1,
@@ -81,35 +85,37 @@ class Fixtures extends CI_Controller
         ];
         $this->db->insert_batch('contracts', $contracts);
 
-        // 2. Organization (Departments)
+        // Organization (Departments)
         $orgs = [
-            ['id' => 0, 'name' => 'Root LMS', 'parent_id' => -1, 'supervisor' => NULL],
-            ['id' => 1, 'name' => 'IT Department', 'parent_id' => 0, 'supervisor' => 1],
-            ['id' => 2, 'name' => 'HR Department', 'parent_id' => 0, 'supervisor' => 2],
+            ['id' => 0, 'name' => 'Root LMS', 'parent_id' => -1, 'supervisor' => null],
+            ['id' => 1, 'name' => 'IT Department', 'parent_id' => 0, 'supervisor' => null],
+            ['id' => 2, 'name' => 'HR Department', 'parent_id' => 0, 'supervisor' => null],
             ['id' => 3, 'name' => 'Staff', 'parent_id' => 0, 'supervisor' => 1],
             ['id' => 4, 'name' => 'Management', 'parent_id' => 3, 'supervisor' => 1],
-            ['id' => 5, 'name' => 'Employees', 'parent_id' => 3, 'supervisor' => 1],
-            ['id' => 6, 'name' => 'Branch 1', 'parent_id' => 3, 'supervisor' => 1],
-            ['id' => 7, 'name' => 'Dept 1', 'parent_id' => 6, 'supervisor' => 1],
-            ['id' => 8, 'name' => 'Dept 2', 'parent_id' => 6, 'supervisor' => 1],
+            ['id' => 5, 'name' => 'Employees', 'parent_id' => 3, 'supervisor' => 3],
+            ['id' => 6, 'name' => 'Branch 1', 'parent_id' => 3, 'supervisor' => 4],
+            ['id' => 7, 'name' => 'Dept 1', 'parent_id' => 6, 'supervisor' => 4],
+            ['id' => 8, 'name' => 'Dept 2', 'parent_id' => 6, 'supervisor' => 4],
             ['id' => 9, 'name' => 'Branch 2', 'parent_id' => 3, 'supervisor' => 1],
             ['id' => 10, 'name' => 'Dept 1', 'parent_id' => 9, 'supervisor' => 1],
             ['id' => 11, 'name' => 'Dept 2', 'parent_id' => 9, 'supervisor' => 1],
-            ['id' => 12, 'name' => 'Branch 3', 'parent_id' => 3, 'supervisor' => 1],
-            ['id' => 13, 'name' => 'Dept 1', 'parent_id' => 12, 'supervisor' => 1],
-            ['id' => 14, 'name' => 'Dept 2', 'parent_id' => 12, 'supervisor' => 1],
+            ['id' => 12, 'name' => 'Branch 3', 'parent_id' => 3, 'supervisor' => 2],
+            ['id' => 13, 'name' => 'Dept 1', 'parent_id' => 12, 'supervisor' => 2],
+            ['id' => 14, 'name' => 'Dept 2', 'parent_id' => 12, 'supervisor' => 2],
         ];
         $this->db->insert_batch('organization', $orgs);
 
-        // 3. Positions
-        $positions = [
-            ['id' => 1, 'name' => 'Director', 'description' => 'Company or Department Director'],
-            ['id' => 2, 'name' => 'Manager', 'description' => 'Team Manager'],
-            ['id' => 3, 'name' => 'Employee', 'description' => 'Regular Employee']
-        ];
+        // Positions
+        for ($ii = 1; $ii < 100; $ii++) {
+            $positions[] = [
+                'id' => $ii,
+                'name' => $this->faker->jobTitle(),
+                'description' => $this->faker->sentence(10)
+            ];
+        }
         $this->db->insert_batch('positions', $positions);
 
-        // 4. Types of Leaves
+        // Types of Leaves
         $types = [
             ['id' => 0, 'name' => 'Compensate', 'deduct_days_off' => 0],
             ['id' => 1, 'name' => 'Paid leave', 'deduct_days_off' => 0],
@@ -124,12 +130,12 @@ class Fixtures extends CI_Controller
 
         echo "Dictionaries and organization inserted.\n";
 
-        // 5. Users
-        // Using Users_model to ensure passwords and random hashes are correctly generated
-        // insertUserByApi(firstname, lastname, login, email, password, role, manager, organization, contract, position, datehired, identifier, language, timezone, ldap_path, active, country, calendar, userProperties, picture)
+        // Users
         $users = [
-            ['id' => 1, 'firstname' => 'Admin', 'lastname' => 'ADMINISTRATOR', 'login' => 'jorani', 'email' => 'jorani@example.org', 'password' => '$2a$08$My0FoP4zalOiS2OM6rsCz.XXozSW1UiqRq4zKup/CC/kOxXDCs9Zm', 'role' => 1, 'manager' => 1, 'organization' => 0, 'contract' => 1, 'position' => 1, 'language' => 'en', 'active' => 1, 'timezone' => 'Europe/Paris'],
-            ['id' => 2, 'firstname' => 'John', 'lastname' => 'DOE', 'login' => 'jdoe', 'email' => 'jdoe@example.org', 'password' => '$2a$08$TU9Syb0FTqcKGwAg5AtH.OLThxcR3p.1gKEcorxvbuqKWTyHQRI7y', 'role' => 1, 'manager' => 1, 'organization' => 0, 'contract' => 1, 'position' => 1, 'language' => 'en', 'active' => 1, 'timezone' => 'Europe/Paris']
+            ['id' => 1, 'firstname' => 'Admin', 'lastname' => 'ADMINISTRATOR', 'login' => 'jorani', 'email' => 'jorani@example.org', 'password' => '$2a$08$My0FoP4zalOiS2OM6rsCz.XXozSW1UiqRq4zKup/CC/kOxXDCs9Zm', 'role' => 9, 'manager' => 1, 'organization' => 0, 'contract' => 1, 'position' => 1, 'language' => 'en', 'active' => 1, 'timezone' => 'Europe/Paris'],
+            ['id' => 2, 'firstname' => 'John', 'lastname' => 'DOE', 'login' => 'jdoe', 'email' => 'jdoe@example.org', 'password' => '$2a$08$TU9Syb0FTqcKGwAg5AtH.OLThxcR3p.1gKEcorxvbuqKWTyHQRI7y', 'role' => 1, 'manager' => 1, 'organization' => 0, 'contract' => 1, 'position' => 2, 'language' => 'en', 'active' => 1, 'timezone' => 'Europe/Paris'],
+            ['id' => 3, 'firstname' => 'Alice', 'lastname' => 'DOE', 'login' => 'alice', 'email' => 'alice@example.org', 'password' => '$2a$08$lzQzTsCKUECKNP6h.r25BOvJStGTzqB5Ljp9KBzXeLiZaYZtjZuxm', 'role' => 1, 'manager' => 1, 'organization' => 0, 'contract' => 1, 'position' => 3, 'language' => 'en', 'active' => 1, 'timezone' => 'Europe/Paris'],
+            ['id' => 4, 'firstname' => 'Bob', 'lastname' => 'Dylan', 'login' => 'bob', 'email' => 'bob@example.org', 'password' => '$2a$08$LwiKZL/pa7UhRjGDCtMpHOU8pQc2Zrkje2cUBRJ1W.tX/SuE40n7a', 'role' => 1, 'manager' => 4, 'organization' => 0, 'contract' => 1, 'position' => 3, 'language' => 'en', 'active' => 1, 'timezone' => 'Europe/Paris']
         ];
         for ($ii = 0; $ii < 100; $ii++) {
             $firstname = $this->faker->firstName;
@@ -137,32 +143,39 @@ class Fixtures extends CI_Controller
             $login = strtolower(substr($firstname, 0, 1) . $lastname);
             $email = "{$login}@example.org";
             $users[] = [
-                'id' => $ii + 3,
+                'id' => $ii + 5,
                 'firstname' => $firstname,
                 'lastname' => $lastname,
                 'login' => $login,
                 'email' => $email,
                 'password' => '$2a$08$hTRW29m7Nyf.fppS32KfquodzgoNSH2ycVQGVHajq3G2.eJznwrnq',
                 'role' => 2,
-                'manager' => $this->faker->numberBetween(1, 2),
+                'manager' => $this->faker->numberBetween(1, 4),
                 'organization' => $this->faker->numberBetween(3, 14),
                 'contract' => 1,
-                'position' => 1,
+                'position' => $this->faker->numberBetween(1, 25),
                 'language' => 'en',
                 'active' => 1,
                 'timezone' => 'Europe/Paris'
             ];
         }
         $this->db->insert_batch('users', $users);
-        echo "Users inserted. (Passwords: jorani for jorani (admin), password for others)\n";
+        echo "Users inserted. (Passwords: jorani for jorani (admin), alice for alice, bob for bob, password for others)\n";
 
-        // 6. Entitled Days (Balances)
+        // Delegations
+        $delegations = [
+            ['id' => 1, 'manager_id' => 2, 'delegate_id' => 4],
+        ];
+        $this->db->insert_batch('delegations', $delegations);
+        echo "Delegations inserted. Bob has no line manager and has delegation from John Doe\n";
+
+        // Entitled Days (Balances)
         $entitledDays = [];
         $currentYear = (int) date('Y');
         // Loop from current year - 3 to current year
         for ($year = $currentYear - 3; $year <= $currentYear; $year++) {
             $entitledDays[] = [
-                'contract' => 1,        // Global to contract
+                'contract' => 1,
                 'employee' => null,
                 'startdate' => $year . '-01-01',
                 'enddate' => $year . '-12-31',
@@ -171,7 +184,7 @@ class Fixtures extends CI_Controller
                 'description' => "Annual base allowance {$year}"
             ];
             $entitledDays[] = [
-                'contract' => 1,        // Global to contract
+                'contract' => 1,
                 'employee' => null,
                 'startdate' => $year . '-01-01',
                 'enddate' => $year . '-12-31',
@@ -180,7 +193,7 @@ class Fixtures extends CI_Controller
                 'description' => "Annual base allowance RTTE {$year}"
             ];
             $entitledDays[] = [
-                'contract' => 1,        // Global to contract
+                'contract' => 1,
                 'employee' => null,
                 'startdate' => $year . '-01-01',
                 'enddate' => $year . '-12-31',
@@ -190,19 +203,32 @@ class Fixtures extends CI_Controller
             ];
         }
         $this->db->insert_batch('entitleddays', $entitledDays);
-        echo "Entitled days inserted. (25 days of paid leave, 5 days of RTTE, 10 days of RTTS per year)\\n";
+        echo "Entitled days inserted. (25 days of paid leave, 5 days of RTTE, 10 days of RTTS per year)\n";
 
-        // 7. Leaves (Requests)
-        // Status: 1=Planned, 2=Requested, 3=Accepted, 4=Rejected
+        // Leaves requests
+        // Status: 1=Planned, 2=Requested, 3=Accepted, 4=Rejected...
         for ($ii = 0; $ii < 1000; $ii++) {
             // Generate a start date between -3 years and +1 year from today
             $startDate = \DateTimeImmutable::createFromMutable(
                 $this->faker->dateTimeBetween('-3 years', '+1 year')
             )->setTime(0, 0, 0);
 
-            // Generate an end date with a maximum span of 20 calendar days
-            $daysToAdd = $this->faker->numberBetween(0, 19);
-            $endDate = $startDate->modify('+' . $daysToAdd . ' days');
+            // Maximum end date allowed by business rule
+            $maxEndDateBySpan = $startDate->modify('+5 days');
+
+            // Last day of the start year
+            $endOfYear = $startDate->setDate(
+                (int) $startDate->format('Y'),
+                12,
+                31
+            );
+
+            // Keep the end date within the same year
+            if ($maxEndDateBySpan > $endOfYear) {
+                $endDate = $endOfYear;
+            } else {
+                $endDate = $maxEndDateBySpan;
+            }
 
             // Compute the number of business days (Monday to Friday), inclusive
             $duration = $this->countBusinessDays($startDate, $endDate);
@@ -211,7 +237,7 @@ class Fixtures extends CI_Controller
                 'startdate' => $startDate->format('Y-m-d'),
                 'enddate' => $endDate->format('Y-m-d'),
                 'status' => $this->faker->numberBetween(LMS_PLANNED, LMS_CANCELED),
-                'employee' => $this->faker->numberBetween(1, 102),
+                'employee' => $this->faker->numberBetween(1, 100),
                 'cause' => $this->faker->words(5, true),
                 'startdatetype' => 'Morning',
                 'enddatetype' => 'Afternoon',
@@ -220,9 +246,9 @@ class Fixtures extends CI_Controller
             ];
         }
         $this->db->insert_batch('leaves', $leaves);
-        echo "Leaves inserted. (1000 leaves)\n";
+        echo "Leaves inserted (1000 leaves). Except for user id > 100.\n";
 
-        // 8. Overtime requests
+        // Overtime requests
         for ($ii = 0; $ii < 100; $ii++) {
             // Generate a start date between -3 years and +1 year from today
             $startDate = \DateTimeImmutable::createFromMutable(
@@ -232,15 +258,15 @@ class Fixtures extends CI_Controller
             $overtimes[] = [
                 'date' => $startDate->format('Y-m-d'),
                 'status' => $this->faker->numberBetween(LMS_PLANNED, LMS_REJECTED),
-                'employee' => $this->faker->numberBetween(1, 102),
+                'employee' => $this->faker->numberBetween(1, 100),
                 'cause' => $this->faker->words(5, true),
                 'duration' => $this->faker->randomFloat(2, 0.25, 1)
             ];
         }
         $this->db->insert_batch('overtime', $overtimes);
-        echo "Overtime requests inserted. (100 overtimes)\n";
+        echo "Overtime requests inserted (100 overtimes). Except for user id > 100.\n";
 
-        // 9. Day offs (Weekends)
+        // Day offs (Weekends)
         $dayoffs = [];
         $currentYear = (int) date('Y');
         $start = new DateTimeImmutable($currentYear . '-01-01');
@@ -273,14 +299,18 @@ class Fixtures extends CI_Controller
         $this->db->insert_batch('dayoffs', $dayoffs);
         echo "Weekends inserted into dayoffs (" . count($dayoffs) . " records)\n";
 
+        // We need this in order to test the API
+        $oauth_clients = [
+            ['client_id' => 'testclient', 'client_secret' => 'testpass', 'redirect_uri' => 'http://fake/']
+        ];
+        $this->db->insert_batch('oauth_clients', $oauth_clients);
+        echo "OAuth clients inserted. (testclient, testpass, http://fake/)\n";
 
-        echo "Entitlements and leaves inserted.\n";
         echo "Fixtures generated successfully!\n";
     }
 
     /**
      * Count business days (Monday to Friday) between two dates, inclusive.
-     *
      * @param \DateTimeInterface $startDate
      * @param \DateTimeInterface $endDate
      * @return float
